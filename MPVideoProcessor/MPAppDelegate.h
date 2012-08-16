53c842d12e0dc87f418ea7103ad27b105df6b255
Haoxiang Li <hli18@host-nonstudentw-158-122.dhcp.stevens-tech.edu>
2012-08-16 18:30:27 -0400

    Initial Commit

---
+++ MPVideoProcessor/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  MPVideoProcessor
+//
+//  Created by Haoxiang Li on 8/16/12.
+//  Copyright (c) 2012 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MPVideoProcessor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MPVideoProcessor
+//
+//  Created by Haoxiang Li on 8/16/12.
+//  Copyright (c) 2012 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
+    }
+}
